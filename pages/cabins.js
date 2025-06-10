df0bf9f74bd937e10feb4e078fbfda7e0c9b07e6
Nikola Ristic <66076379+DzoniBatana@users.noreply.github.com>
2025-06-10 23:37:49 +0200

    update

--- pages/cabins.js
+++ pages/cabins.js
@@ -1,4 +1,13 @@
-function Cabins(){
+import { getCabins } from "@/lib/data-service";
+
+export async function getStaticProps(){
+    const cabins = await getCabins();
+    return {props: {cabins }};
+}
+
+function Cabins({cabins}){
+    console.log(cabins);
+
     return (
         <div>
             Cabins page
